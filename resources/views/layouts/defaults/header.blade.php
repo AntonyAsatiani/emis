ae1aa60862cce22e3e3a52b66e1f803b55051adf
re3p3r <anton.asatiani@gmail.com>
2019-10-05 18:04:56 +0400

    linking pages

--- resources/views/layouts/defaults/header.blade.php
+++ resources/views/layouts/defaults/header.blade.php
@@ -2,7 +2,7 @@
             <div class="container flex-container">
                 <div class="left-main-header">
                     <ul>
-                        <li><a class="home-link" href="index.html">Home</a></li>
+                        <li><a class="home-link" href="{{url('/')}}">Home</a></li>
                     </ul>
                 </div>
                 <div class="right-main-header">
@@ -10,15 +10,20 @@
                         <input class="search-input" type="text" placeholder="Search" name="search">
                     </form>
                     <ul>
-                        <li><a href="registration.html">Register</a></li>
-                        <li><a href="login.html">Login</a></li>
+                        <li><a href="{{url('register')}}">Register</a></li>
+                        <li><a href="{{url('login')}}">Login</a></li>
                         <li class="dropdown-li" onClick="toggleUserDropdown();"><span class="user-image"><img  src="{{URL::asset('images/profile_default2.png') }}"></span><span>Username</span><i id="dropdown-icon" class="fa fa-caret-down"></i>
                             <ul id="user-dropdown-ul">
-                                <li><a href="profile.html">Profile</a></li>
+                                <li><a href="{{url('user/profile')}}">Profile</a></li>
                                 <!-- <li><a href="">Gallery</a></li>
                                 <li><a href="">Settings</a></li> -->
-                                <li><a href="logout">Sign Out</a></li>
+                                <li><a href="{{url('logout')}}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}</a></li>
                             </ul>
+                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
                         </li>
                     </ul>
                 </div>
